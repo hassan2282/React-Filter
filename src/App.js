803633e235a99c3f534762187679899e5c532f44
سید حسن تقوی <taghavey.hassan@gmail.com>
2025-02-12 23:58:21 +0330

    add some code to project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { useEffect } from 'react';
 import './App.css';
 import { Icon } from 'react-icons-kit'
 import { home } from 'react-icons-kit/icomoon/home'
 
 function App() {
+
+  useEffect(()=> {
+    return (
+      console.log(first)
+    ) 
+  },[]);
+
   return (
-    <div>
-      <Icon size={32} icon={home} className='p-2 text-red-400'/>
+    <div className='flex flex-col w-screen h-screen bg-indigo-100' dir='rtl'>
+        <input type='search' className='rounded-md bg-indigo-50 w-[30%] shadow-sm p-2'/>
+        <div className='flex flex-col items-start justify-center w-full h-96 bg-indigo-200'>
+
+        </div>
     </div>
   );
 }
